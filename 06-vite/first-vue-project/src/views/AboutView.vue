751d8f9e5760f85969eecde73c45da2b376acd04
Pauline <huby.pauline@gmail.com>
2022-12-01 12:00:12 -0500

    Ajout mixins cours 6 vite

---
+++ 06-vite/first-vue-project/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<script>
+  import flash from '@/mixins/flash';
+
+  export default {
+    mixins: [flash]
+  }
+</script>
+
+<template>
+  <div class="about">
+    <p>
+      <button @click="flash('It works!')">Click me</button>
+    </p>
+  </div>
+</template>
+
+
---
+++ 06-vite/first-vue-project/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+
+let message = $ref("Hello world"); // ajout ds vite.config.js pour utiliser le $ sur ref
+
+setTimeout(() => {
+  message = "I have been changed";
+}, 2000);
+
+let doSomething = () => {
+  alert('Bouh!');
+}
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <p>{{message}}</p>
+    <input type="text" v-model="message">
+    <button @click="doSomething">Click me</button>
+  </main>
+</template>
