7a3309c22b0577b4451bece93d9b7a3d258fa705
Pauline <huby.pauline@gmail.com>
2022-12-22 12:05:06 -0500

    Ajout scroll smooth

---
+++ 06-vite/biero-project/src/components/ScrollLink.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class='hero-title-ctn'>
+        <a :href="href" @click.prevent="scroll">
+            <slot></slot>
+        </a>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    href: String
+})
+
+function scroll() {
+    document.querySelector(props.href).scrollIntoView({ behavior: 'smooth' });
+}
+
+</script>
--- 06-vite/biero-project/src/views/HomeView.vue
+++ 06-vite/biero-project/src/views/HomeView.vue
@@ -1,17 +1,24 @@
 <script setup>
 import { RouterLink } from "vue-router";
 import 'animate.css';
+import ScrollLink from "../components/ScrollLink.vue";
 
+
+// function scroll(href) {
+//     document.querySelector(href)
+//       .scrollIntoView({ behavior: 'smooth' });
+// }
 
 </script>
 
 <template>
 
     <main class='hero-wrapper'>
+      <ScrollLink href="#div-bottom" >Clique ici pour un scroll smooooth</ScrollLink>
       <div class='hero-img-ctn animate__animated animate__fadeIn'>
         <img src='../assets/img/hero-img.jpg' alt='' />
       </div>
-      <div class='hero-title-ctn'>
+      <div class='hero-title-ctn' id="div-bottom">
         <h1>Vous cherchez une bière ?</h1>
         <h2>Nous l'avons sans doute goutée pour vous.</h2>
       </div>
